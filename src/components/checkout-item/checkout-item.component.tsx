0fef4fa950cd6f9dbcf19188a0d782d33c4f20ff
kouki030 <emmashih15@gmail.com>
2022-07-26 01:56:49 -0500

    converting into textscript

---
+++ src/components/checkout-item/checkout-item.component.tsx
@@ -0,0 +1,58 @@
+import { FC } from 'react';
+import { useDispatch, useSelector } from 'react-redux/es/exports';
+
+import { selectCartItems } from '../../store/cart/cart.selector';
+import {
+    clearItemFromCart,
+    addItemToCart,
+    removeItemToCart
+} from '../../store/cart/cart.action';
+
+import { CartItem } from '../../store/cart/cart.types';
+
+import {
+    CheckoutItemContainer,
+    ImageContainer,
+    BaseSpan,
+    Quantity,
+    Arrow,
+    Value,
+    RemoveButton,
+} from './checkout-item.styles';
+
+type CheckoutItemProps = {
+    cartItem: CartItem,
+}
+
+const CheckoutItem: FC<CheckoutItemProps> = ({ cartItem }) => {
+    const { name, imageUrl, price, quantity } = cartItem;
+    const cartItems = useSelector(selectCartItems);
+    const dispatch = useDispatch();
+
+    const clearItemHandler = () => dispatch(clearItemFromCart(cartItems, cartItem));
+    const addItemHandler = () => dispatch(addItemToCart(cartItems, cartItem));
+    const removeItemHandler = () => dispatch(removeItemToCart(cartItems, cartItem));
+
+    return (
+        <CheckoutItemContainer>
+            <ImageContainer>
+                <img src={imageUrl} alt={`${name}`} />
+            </ImageContainer>
+            <BaseSpan>{name}</BaseSpan>
+            <Quantity>
+                <Arrow onClick={removeItemHandler}>
+                    &#10094;
+                </Arrow>
+                <Value>{quantity}</Value>
+                <Arrow onClick={addItemHandler}>
+                    &#10095;
+                </Arrow>
+            </Quantity>
+            <BaseSpan>{price}</BaseSpan>
+            <RemoveButton onClick={clearItemHandler}>&#10005;</RemoveButton>
+        </CheckoutItemContainer>
+    );
+};
+
+
+export default CheckoutItem;
---
+++ src/routes/category/category.component.tsx
@@ -0,0 +1,46 @@
+import { useState, useEffect, Fragment } from 'react';
+import { useSelector } from 'react-redux/es/exports';
+import { useParams } from 'react-router-dom';
+
+import ProductCard from '../../components/product-card/product-card.component';
+import Spinner from '../../components/spinner/spinner.component';
+
+import { selectCategoriesIsLoading, selectCategoriesMap } from '../../store/categories/categories.selector';
+
+import {
+    CategoryContainer,
+    CategoroyTitle,
+} from './category.styles';
+
+type CategoryRouteParams = {
+    category: string;
+}
+
+const Category = () => {
+    const { category } = useParams<keyof CategoryRouteParams>() as CategoryRouteParams;
+    const categoriesMap = useSelector(selectCategoriesMap);
+    const isLoading = useSelector(selectCategoriesIsLoading)
+    const [products, setProducts] = useState(categoriesMap[category]);
+
+    useEffect(() => {
+        setProducts(categoriesMap[category]);
+    }, [category, categoriesMap])
+
+    return (
+        <Fragment>
+            <CategoroyTitle>{category.toUpperCase()}</CategoroyTitle>
+            {isLoading ? (
+                <Spinner />
+            ) : (
+                <CategoryContainer>
+                    {products &&
+                        products.map((product) => (
+                            <ProductCard key={product.id} product={product} />
+                        ))}
+                </CategoryContainer>
+            )}
+        </Fragment>
+    );
+};
+
+export default Category;
